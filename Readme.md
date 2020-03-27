c0e646d127fc1b99d56a53c0a455eabae65cec0b
Osman Yuksel <yuxel@sonsuzdongu.com>
2020-03-27 19:45:55 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,39 @@
+# Zavadanak
+
+A simple python script to crawl pages recursively using headless chrome
+and list some information like Time to first byte, dom load and page load
+
+
+## Usage
+
+python zavadanak.py http://[YOUR_URL]
+
+<pre>
+                                 `` `                                            
+                             `.-....-.``                                         
+                           `:/++o++//::-``
+                          `-//+ooosso++/-`                                                   `---::/
+                          `-+ooooo+/oo+o/`                                            `.-:/ossssoooo
+                          -:+++oo++//o++/.                                       .-/osyhhyyssso+/-..
+                          ./+osso+++:oso/.                               ``.---/oyddddhhyyyyso+.
+                          `:/os+///+::/o/-                           `-/oys++++osyyhddoooosssyys/`
+                           `/+++oosso/+/:.```       `         `.//+oshhhhsooooosyssoo+:--:/++oyys+-
+                        .///:::/++++//:---+/////+oosso+/:::/syyyysyhddhdy/ssoosyyyyyyyo. `.-:osys/.
+                     `-osss+///:::/::--:/ssoysyyyhyhhyyssyhdyhhyyhhdddhh+/yysso+oo++++-`    .sss+-`
+                 `:/oossyyyysoosoo+++oyhysssyyyhhyhhyyyyyhdhdhyhhhddhdhy/:ysooyhyyy`       `+ss+-:.:
+               `/ssyyyyyyyhhyyhhhhhhhhhhhsyyhyhhhyhyyyyyhhhhhyyyhhhhhhhy/-/shddhyy-        .oso:/-//
+              `syyyyyyyyyyyhhyhhhhhhhhhhhsyyhhhdhhyyyyyyyyyyyyyyyyyhyhyho-/yddhyy:          `-++- ..
+             `oyhhhhhhhhhhhhhhhhhhhhhyhhhyhhhhhhdhsyyyssoosssssssyyyyyyhyo:/yyss:              `
+             /yyhhhhhhhhhhhhhhhhhhhhhyhhhyhhhhhydhsyyys/--......--::::--..-:+/-`
+            `yyyhhhhhhhhhhhhhhhhhhhddhhhhhhhhhhyhhoyos:           
+            /yyyhhhhhhhhhhhhhhhhhdhhdhhhhhhhhhhshh+so/            
+           `syysyyhyhhhhhhhhhhhhhhhhhhhhhhhhhhhsyh+o+.            
+           :yyysyyysyhhhhhhddhhhhhhhhhhhhhhhhhyyyh+o/              
+           oyyssyysosyyhhhhhhhhhhhhhhhhhhhyhhhyyyh++-             
+          `sysssyssosssyyhhhhhhhhyhhhhhhhhhhhyssyh+/.             
+          /ssssssssoosssyyyhhhhhhhyhhhhhhhhhyyssyh//`              
+          +ssssssoo//ssssyyyhhhhhhhhhhhhhhhyyssoyd/+`
+</pre>
+
+
+
---
+++ zavadanak.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Python 2.7.6
+
+# pip install beautifulsoup selenium
+
+"""
+  A simple script to crawl pages of given url using a 
+  headless browser and getting the page load stats
+
+  Usage: python zavadanak.py https://www.sonsuzdongu.com
+"""
+
+import os
+import sys
+
+from BeautifulSoup import BeautifulSoup
+from urlparse import urlparse
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+        
+chromeDriverPath = "/usr/local/bin/chromedriver"
+
+options = Options()
+options.headless = True
+
+
+def crawl_urls(driver, already_crawled_urls, url, domain):
+    url_list = list()
+    already_crawled_urls.append(url)
+
+    driver.get(url)
+
+    responseStart = driver.execute_script("return window.performance.timing.responseStart")
+    responseEnd = driver.execute_script("return window.performance.timing.responseEnd")
+    fetchStart = driver.execute_script("return window.performance.timing.fetchStart")
+    domInteractive = driver.execute_script("return window.performance.timing.domInteractive")
+    loadEventStart = driver.execute_script("return window.performance.timing.loadEventStart")
+
+    ttfb = responseStart - fetchStart
+    domReady = ttfb + domInteractive - responseStart
+    pageLoad = ttfb + loadEventStart - responseStart
+
+    print("\n---------\n")
+    print("URL: %s" % url)
+    print("TTFB: %d" % ttfb)
+    print("Dom ready: %d" % domReady)
+    print("Page load: %d" % pageLoad)
+
+    html = driver.page_source.encode("utf-8")
+    soup = BeautifulSoup(html)
+    urls = soup.findAll("a")
+
+    for a in urls:
+        if (a.get("href")) and urlparse(a.get("href")).netloc == domain :
+            url_list.append(a.get("href"))
+
+    # remove duplicates
+    for page in set(url_list):
+        if (page not in already_crawled_urls):
+            crawl_urls(driver, already_crawled_urls, page, domain)
+
+    else:
+        return already_crawled_urls, url_list
+
+
+if __name__ == "__main__":
+    try:
+        parent_url = sys.argv[1]
+        print ("Crawling : %s" % parent_url)
+    except :
+        print ("You need to send a base url")
+        sys.exit(1)
+    try:
+        domain = urlparse(parent_url).netloc
+        if not domain:
+            raise
+    except: 
+        print ("Not a valid url")
+        sys.exit(1)
+
+    driver = webdriver.Chrome(executable_path=chromeDriverPath,   options=options)  
+
+    already_crawled_urls = list()
+    already_crawled_urls = crawl_urls(driver, already_crawled_urls, parent_url, domain)
+    driver.quit()
